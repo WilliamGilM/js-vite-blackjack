3d3a3d6eee72cf22a1db5da48375741f0dd6ecac
William Gil <williamgm94@gmail.com>
2023-04-08 15:33:39 -0600

    crear-carta-htrml updated

--- src/blackjack/usecases/crear-carta-html.js
+++ src/blackjack/usecases/crear-carta-html.js
@@ -7,7 +7,10 @@
 export const crearCartaHTML = (carta, turno, divCartasJugadores) => {
   if (!carta) throw new Error("La carta es un argumento obligatorio");
   const imgCarta = document.createElement("img");
-  imgCarta.src = `/assets/cartas/cartas/${carta}.png`;
+  imgCarta.src = `${
+    import.meta.env.VITE_URL_REPOSITORIO_GIT
+  }assets/cartas/cartas/${carta}.png`;
+  console.log("src", imgCarta.src);
   imgCarta.classList.add("carta");
   divCartasJugadores[turno].append(imgCarta);
 };
